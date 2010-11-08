578a76401bf110a0c9e4308fa36af502873bf192
Jonas Alves <jonasfa@gmail.com>
2010-11-08 14:17:45 -0200

    adicionando imagem do estado focused  ao botão 'Calcular'

--- gasosa/gen/br/com/jera/gasosa/R.java
+++ gasosa/gen/br/com/jera/gasosa/R.java
@@ -16,17 +16,18 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int buttonbg=0x7f020001;
-        public static final int buttonbg2=0x7f020002;
-        public static final int calc_button=0x7f020003;
-        public static final int ethanol=0x7f020004;
-        public static final int gas=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int logo=0x7f020007;
-        public static final int logo_focused=0x7f020008;
-        public static final int logo_pressed=0x7f020009;
-        public static final int logo_unfocused=0x7f02000a;
-        public static final int top=0x7f02000b;
-        public static final int valor=0x7f02000c;
+        public static final int buttonbg1=0x7f020002;
+        public static final int buttonbg2=0x7f020003;
+        public static final int calc_button=0x7f020004;
+        public static final int ethanol=0x7f020005;
+        public static final int gas=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int logo=0x7f020008;
+        public static final int logo_focused=0x7f020009;
+        public static final int logo_pressed=0x7f02000a;
+        public static final int logo_unfocused=0x7f02000b;
+        public static final int top=0x7f02000c;
+        public static final int valor=0x7f02000d;
     }
     public static final class id {
         public static final int alcool_price=0x7f070000;
